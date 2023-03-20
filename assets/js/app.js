8275abd38c588809e1d899dad5419a7edce3285e
mahaUser2 <75357482+mahaUser2@users.noreply.github.com>
2023-03-20 07:56:35 +0100

    A few adjustments in the comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-// 1. Der skal, med brug af javascript, indsættes 3 skills mere. Altså følgende html:
+// 1. Der skal, med brug af javascript, indsættes 3 skills mere, altså nedenstående html. Se mere i punkt 1.1 - 1.6 under
 /*
     <div>
         <span>CSS:</span>
@@ -12,11 +12,11 @@
 // 1.3 Anvend document.textContent til at indsætte data i 2*span(se ovenfor for indhold)
 // 1.4 Anvend .setAttribute() til at sætte de tre attributter i meter
 // 1.5 Anvend .append() til at indsætte <div> i .mySkills
-// 1.6 Anvend .append() til at indsætte 2*span og 1*meter i div
+// 1.6 Anvend .append() til at indsætte 2*span og 1*meter i <div>
 
 // 2. Anvend .cloneNode() til at klone den <li> med "Mit portfolio" og indsætte den to gange nedenfor (Ja, så kommer der til at stå "Mit portfolio" tre gange - pyt med det:) )
 
 // 3. I footeren er der en div med class="oval". Anvend .style-objektet til at give den følgende:
-// width: 40px, height: 40px, background: pink, border-radius: 50%
+// width: 40px, height: 40px, background-color: pink, border-radius: 50%
 
 // 4. Se i dit .css dokument nederst at der er en class kaldet .niceStyling. Anvend .classList.add() til at sætte denne klasse på .oval2
